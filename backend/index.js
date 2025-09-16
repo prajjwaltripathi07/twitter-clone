d8d28c95ed14aa0f2c999ab6a4847fa75213ccd4
PRAJJWAL TRIPATHI <prajjwaltripathi70@gmail.com>
2025-09-16 16:29:24 +0530

    Initial commit of twitter-clone

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,10 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 3000;
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173"],
+  credentials: true
+}));
 app.use(express.json());
 
 const url =
